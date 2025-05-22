6147cad3b7110f4ba49e8d128d706b4686d1180a
anniep8911 <pjatopazz@gmail.com>
2025-05-22 16:55:49 +0900

    fix: del

--- storybook-static/assets/Button.stories-BbhXJK4x.js
+++
@@ -1,6 +0,0 @@
-import{M as t}from"./Button-D2_MwLtr.js";import"./vue.esm-bundler-DC5g5G_I.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const p={title:"Compoenets/Button",component:t,tags:["autodocs"],argTypes:{size:{control:{type:"select"},options:["small","large"]},btnType:{control:{type:"select"},options:["normal","sns","sharp"]},backgroundColor:{control:"color"},disabled:Boolean}},o={args:{label:"원픽 챌린지 참여",disabled:!1}};var e,r,s;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    label: '원픽 챌린지 참여',
-    disabled: false
-  }
-}`,...(s=(r=o.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};const c=["primary"];export{c as __namedExportsOrder,p as default,o as primary};

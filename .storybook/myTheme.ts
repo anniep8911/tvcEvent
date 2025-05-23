1d5db9d3fe3c01a516bed2bd739ebc7cd7c2d307
anniep8911 <pjatopazz@gmail.com>
2025-05-23 15:42:43 +0900

    fix: 로고 위치 경로설정

--- .storybook/myTheme.ts
+++ .storybook/myTheme.ts
@@ -7,7 +7,7 @@ export default create({
     base: 'light',
     brandTitle: '박정아 스토리북',
     brandUrl: './',
-    brandImage: isDev ? '/logo.png' : 'https://github.com/anniep8911/tvcEvent/blob/main/public/logo.png',
+    brandImage: isDev ? '/logo.png' : 'https://github.com/anniep8911/tvcEvent/blob/main/public/logo.png?raw=true',
     colorPrimary: '#A7D5C5', // 메인 컬러
     colorSecondary: '#4cb177', // 액센트 컬러
 });

be92dd62bb6e5b5ac79bd702cc4b54fad2bc2f1d
anniep8911 <pjatopazz@gmail.com>
2025-05-23 15:36:29 +0900

    fix: 로고 위치 경로설정

--- .storybook/myTheme.ts
+++ .storybook/myTheme.ts
@@ -1,12 +1,13 @@
 // .storybook/myTheme.ts
 
 import { create } from '@storybook/theming/create';
+const isDev = process.env.NODE_ENV === 'development';
 
 export default create({
     base: 'light',
     brandTitle: '박정아 스토리북',
     brandUrl: './',
-    brandImage: '/logo.png', // public 폴더에 있는 로고
+    brandImage: isDev ? '/logo.png' : '/storybook-static/logo.png',
     colorPrimary: '#A7D5C5', // 메인 컬러
     colorSecondary: '#4cb177', // 액센트 컬러
 });
